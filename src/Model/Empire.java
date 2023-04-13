ad0f98d4d2d0dc74fc5482b8e8f109aded4dc205
Sadeghdrr <sadegh.sg.sn@gmail.com>
2023-04-13 23:44:19 +0430

    Add SRC Folder

---
+++ src/Model/Empire.java
@@ -0,0 +1,9 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Empire {
+    private User owner;
+    private static final ArrayList<String> popularityFactors = new ArrayList<>();
+
+}
